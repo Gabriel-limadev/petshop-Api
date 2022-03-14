28aadd696408a05c620921778a73dc7ef193076b
Gabriel-limadev <gabrielvitor180701@gmail.com>
2022-03-14 14:50:08 -0300

    Finished CRUD

--- api/routers/providers/TableProvider.js
+++ api/routers/providers/TableProvider.js
@@ -2,5 +2,35 @@ const Model = require('./ModelTableProviders')
 module.exports = {
     list () {
         return Model.findAll()
+    },
+    insert(provider){
+        return Model.create(provider)
+    },
+    async getById(id){
+        const foundProvider = await Model.findOne({
+            "where": {
+                "id": id
+            }
+        })
+
+        if(!foundProvider){
+            throw new Error('Provider not found')
+        }
+        return foundProvider
+    },
+    update(id, dataforupdate){
+        return Model.update(
+            dataforupdate,
+            {
+                where : {"id": id}
+            }
+        )
+    },
+    delete(id){
+        return Model.destroy(
+            {
+                where : {"id": id}
+            }
+        )
     }
 }
